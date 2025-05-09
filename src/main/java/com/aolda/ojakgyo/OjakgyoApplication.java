e7a00cb28911774eda29d8276db3c45b39f186f4
jjjjjk12 <jjjjjk12@naver.com>
2025-05-09 18:34:06 +0900

    파일 추가

---
+++ src/main/java/com/aolda/ojakgyo/OjakgyoApplication.java
@@ -0,0 +1,13 @@
+package com.aolda.ojakgyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjakgyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjakgyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aolda/ojakgyo/OjakgyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aolda.ojakgyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjakgyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
