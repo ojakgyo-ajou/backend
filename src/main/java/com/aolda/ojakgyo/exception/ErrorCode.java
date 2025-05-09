d8e141dbb804094dc29e53c75599952859bde135
jjjjjk12 <jjjjjk12@naver.com>
2025-05-09 20:06:16 +0900

    인증인가 추가

--- src/main/java/com/aolda/ojakgyo/exception/ErrorCode.java
+++ src/main/java/com/aolda/ojakgyo/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // User
-    INVALID_USER_INFO(HttpStatus.BAD_REQUEST, "잘못된 회원 정보입니다");
+    INVALID_USER_INFO(HttpStatus.BAD_REQUEST, "잘못된 회원 정보입니다"),
+    USER_ARE_EXIST(HttpStatus.BAD_REQUEST, "이미 존재하는 회원입니다"),;
 
     private final HttpStatus status;
     private final String message;
